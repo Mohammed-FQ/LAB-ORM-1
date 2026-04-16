2cd99b04005d70b88cf1df8e3cf9d4b0f8cc9f06
Mohammed <mohammed@Mohammeds-MacBook-Pro.local>
2026-04-16 08:52:02 +0300

    minor fixed

--- Blogger/main/urls.py
+++ Blogger/main/urls.py
@@ -5,5 +5,10 @@ app_name = 'main'
 
 urlpatterns = [
     path('', views.home_view, name='home_view'),
+    path('posts/', views.posts_view, name='posts_view'),
+    path('create/', views.create_post_view, name='create_post_view'),
+    path('posts/<int:post_id>/', views.post_detail_view, name='post_detail_view'),
+    path('posts/<int:post_id>/edit/', views.edit_post_view, name='edit_post_view'),
+    path('posts/<int:post_id>/delete/', views.delete_post_view, name='delete_post_view'),
     path('mode/<mode>/', views.mode_view, name='mode_view'),
 ]
--- Blogger/main/views.py
+++ Blogger/main/views.py
@@ -1,26 +1,63 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpRequest
 from .models import Post
 from .check_post import is_bad_word
 
-def home_view(request:HttpRequest):
+def home_view(request: HttpRequest):
+    return redirect('main:posts_view')
+
+def posts_view(request: HttpRequest):
     posts = Post.objects.all()
+    return render(request, 'main/posts.html', {"posts": posts})
+
+def post_detail_view(request: HttpRequest, post_id: int):
+    post = get_object_or_404(Post, id=post_id)
+    return render(request, 'main/post_detail.html', {"post": post})
+
+def edit_post_view(request: HttpRequest, post_id: int):
+    post = get_object_or_404(Post, id=post_id)
+
+    if request.method == "POST":
+        title = request.POST['title']
+        content = request.POST['content']
+
+        if is_bad_word(title):
+            return render(request, 'main/edit_post.html', {"post": post, "safe_title": False})
+        if is_bad_word(content):
+            return render(request, 'main/edit_post.html', {"post": post, "safe_content": False})
+
+        image = request.FILES.get('image')
+        post.title = title
+        post.content = content
+        if image:
+            post.image = image
+        post.save()
+        return redirect('main:post_detail_view', post_id=post.id)
+
+    return render(request, 'main/edit_post.html', {"post": post})
+
+def delete_post_view(request: HttpRequest, post_id: int):
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == "POST":
+        post.delete()
+        return redirect('main:posts_view')
+    return redirect('main:post_detail_view', post_id=post.id)
+
+def create_post_view(request: HttpRequest):
     if request.method == "POST":
         title = request.POST['title']
         content = request.POST['content']
         if is_bad_word(title):
-            print("not safe 1")
-            return render(request, 'main/home.html',{"posts": posts, "safe_title": False})
+            return render(request, 'main/create_post.html', {"safe_title": False})
         if is_bad_word(content):
-            print("not safe 2")
-            return render(request, 'main/home.html',{"posts": posts, "safe_content": False})
+            return render(request, 'main/create_post.html', {"safe_content": False})
         image = request.FILES.get('image')
         new_post = Post(title=title, content=content)
         if image:
             new_post.image = image
         new_post.save()
-        return render(request, 'main/home.html',{"posts": posts, "safe_title": True, "safe_content": True})
-    return render(request, 'main/home.html',{"posts": posts})
+        return redirect('main:posts_view')
+    return render(request, 'main/create_post.html')
 
 def mode_view(request: HttpRequest, mode: str):
     response = redirect(request.GET.get('next', '/'))
